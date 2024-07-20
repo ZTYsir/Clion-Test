649a4f2d1efb9d917b48baeb7c23864c5830b167
ZTYsir <974808867@qq.com>
2024-07-21 03:58:13 +0800

    swap add msg

--- src/swap.cpp
+++ src/swap.cpp
@@ -1,10 +1,14 @@
 //
 // Created by ztyer on 2024/6/14.
 //
+#include <iostream>
 #include "swap.h"
+using namespace std;
 // 这是一个交换函数
+
 void swap(int &a,int &b){
     int tmp=a;
     a=b;
     b=tmp;
+    cout<<"交换完成"<<endl;
 }
